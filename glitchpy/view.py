c2037c195e5ce14c3821ded122d0a0d71a2d72c4
gusbusdraws <cgusbecker@gmail.com>
2024-01-04 14:42:53 -0500

    Add option for plotting thresholds in 'histogram()'

--- glitchpy/view.py
+++ glitchpy/view.py
@@ -31,13 +31,35 @@ def channels(
         axes[i].set_axis_off()
     return fig, axes
 
-def histogram(img, nbins=256, channel_labels=None):
+def histogram(
+    img,
+    nbins=256,
+    channel_thresholds=None,
+    channel_labels=None,
+    return_hist=False
+):
+    colors = ['C3', 'C2', 'C0']
     if channel_labels == None:
         channel_labels = [0, 1, 2]
+    # Compile single values into nested list by channel
+    if channel_thresholds is not None:
+        for i in range(3):
+            if not isinstance(channel_thresholds[i], list):
+                channel_thresholds[i] = [channel_thresholds[i]]
     fig, ax = plt.subplots()
+    hist_list = []
+    bins_list = []
     for chan in range(img.shape[-1]):
         hist, bins = np.histogram(img[..., chan], bins=nbins)
-        ax.plot(bins[1:], hist, c=f'C{chan}', label=channel_labels[chan])
+        hist_list.append(hist)
+        bins_list.append(bins)
+        ax.plot(bins[1:], hist, c=colors[chan], label=channel_labels[chan])
+        if channel_thresholds is not None:
+            for thresh in channel_thresholds[chan]:
+                ax.axvline(thresh, c=colors[chan])
         ax.legend()
-    return fig, ax
+    if return_hist:
+        return hist_list, bins_list
+    else:
+        return fig, ax
 
