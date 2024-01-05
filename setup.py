52f072df28698a19b1fe81f606e9a7131b1b9d21
gusbusdraws <cgusbecker@gmail.com>
2024-01-05 09:35:51 -0500

    Increment version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="glitchpy",
-    version="0.0.1",
+    version="0.0.2",
     install_requires=[
         'imageio',
         'matplotlib',
