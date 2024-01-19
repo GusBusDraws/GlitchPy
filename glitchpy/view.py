a150f767622c1f0caee3563903174cc58a30f9af
gusbusdraws <cgusbecker@gmail.com>
2024-01-19 14:03:41 -0500

    Add 'histogram_split'

--- glitchpy/view.py
+++ glitchpy/view.py
@@ -139,3 +139,35 @@ def histogram(
     else:
         return fig, ax
 
+def histogram_split(
+    img,
+    nbins=256,
+    channel_thresholds=None,
+    channel_labels=None,
+    return_hist=False
+):
+    colors = ['C3', 'C2', 'C0']
+    if channel_labels == None:
+        channel_labels = [0, 1, 2]
+    # Compile single values into nested list by channel
+    if channel_thresholds is not None:
+        for i in range(3):
+            if not isinstance(channel_thresholds[i], list):
+                channel_thresholds[i] = [channel_thresholds[i]]
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(24, 5))
+    hist_list = []
+    bins_list = []
+    for chan in range(img.shape[-1]):
+        hist, bins = np.histogram(img[..., chan], bins=nbins)
+        hist_list.append(hist)
+        bins_list.append(bins)
+        axes[chan].plot(bins[1:], hist, c=colors[chan], label=channel_labels[chan])
+        if channel_thresholds is not None:
+            for thresh in channel_thresholds[chan]:
+                axes[chan].axvline(thresh, c=colors[chan])
+        axes[chan].legend()
+    if return_hist:
+        return hist_list, bins_list
+    else:
+        return fig, axes
+
